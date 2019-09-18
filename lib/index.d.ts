4d4ef24a0164a0adf61514cfd14da14d7f93df7d
Fati Chen <chen@lirmm.fr>
2019-09-18 09:41:04 +0200

    :bookmark: :package: 0.2.0 github release

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,10 @@
-import { Point } from 'agora-graph';
-export declare const forceTransfer: import("agora-algorithm").Function<{
+import { Point, Algorithm } from 'agora-graph';
+export declare const forceTransfer: import("agora-graph").Function<{
     padding: number;
     seed: Point;
 }>;
+export declare const ForceTransferAlgorithm: Algorithm<{
+    padding: number;
+    seed: Point;
+}>;
+export default ForceTransferAlgorithm;
